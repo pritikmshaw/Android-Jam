81b69e01fc2e46260bb34436ef616c6b188fde3b
pritikmshaw <pritikmshaw18@gmail.com>
2021-01-03 21:06:03 +0530

    random numbers generated

--- Rolldice/app/src/main/java/com/example/rolldice/MainActivity.kt
+++ Rolldice/app/src/main/java/com/example/rolldice/MainActivity.kt
@@ -2,7 +2,7 @@ package com.example.rolldice
 
 import android.os.Bundle
 import android.widget.Button
-import android.widget.Toast
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
@@ -11,8 +11,23 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         val rollButton : Button = findViewById(R.id.button)
         rollButton.setOnClickListener {
-            val toast = Toast.makeText(this , "Dice Rolled" ,Toast.LENGTH_SHORT)
-            toast.show()
+            rollDice()
+            //val toast = Toast.makeText(this , "Dice Rolled" ,Toast.LENGTH_SHORT)
+            //toast.show()
+            //val resultTextView : TextView = findViewById(R.id.textView)
+            //resultTextView.text = "6"
         }
+    }
+
+    private fun rollDice() {
+        val dice = Dice(6)
+        val diceRoll = dice.roll()
+        val resultTextView : TextView = findViewById(R.id.textView)
+        resultTextView.text = diceRoll.toString()
+    }
+}
+class Dice (private val numSide: Int) {
+    fun roll(): Int {
+        return (1..numSide).random()
     }
 }
